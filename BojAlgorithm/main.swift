3a3cf5493ab44ac2629fa477192c3134406c88e3
윤형찬 <72960152+velmash@users.noreply.github.com>
2024-09-26 09:06:45 +0900

    :tada: Project 생성

---
+++ BojAlgorithm/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  BojAlgorithm
+//
+//  Created by 윤형찬 on 9/26/24.
+//
+
+import Foundation
+
+Q1000.solved()
---
+++ Class_One/1000.swift
@@ -0,0 +1,14 @@
+//
+//  1000.swift
+//  BojAlgorithm
+//
+//  Created by 윤형찬 on 9/26/24.
+//
+
+import Foundation
+
+final class Q1000 {
+    static func solved() {
+        print("Test Class")
+    }
+}
