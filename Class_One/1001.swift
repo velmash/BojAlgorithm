ed1e017ad909ed4c999877c34d9e9400ba5a4a7b
윤형찬 <72960152+velmash@users.noreply.github.com>
2024-09-30 09:46:24 +0900

    :bug: Warning 제거

--- Class_One/1001.swift
+++ Class_One/1001.swift
@@ -9,7 +9,7 @@ import Foundation
 
 final class Q1001 {
     static func solved() {
-        var input = readLine()!.split(separator: " ").map { Int($0)! }
+        let input = readLine()!.split(separator: " ").map { Int($0)! }
         print(input[0] - input[1])
     }
 }
